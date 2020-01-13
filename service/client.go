f4763be760a1a69a1e07050e14c9db830b53b361
wenzuochao <wb-wzc505509@alibaba-inc.com>
2020-01-13 13:39:56 +0800

    add config tp client

--- service/client.go
+++ service/client.go
@@ -16,19 +16,21 @@ import (
 
 // client is for calling oss api
 type BaseClient struct {
-	RegionId       string `json:"RegionId" xml:"RegionId"`
-	Protocol       string `json:"Protocol" xml:"Protocol"`
-	Endpoint       string `json:"Endpoint" xml:"Endpoint"`
-	UserAgent      string `json:"UserAgent" xml:"UserAgent"`
-	SecurityToken  string `json:"SecurityToken" xml:"SecurityToken"`
-	ReadTimeout    int    `json:"ReadTimeout" xml:"ReadTimeout"`
-	ConnectTimeout int    `json:"ConnectTimeout" xml:"ConnectTimeout"`
-	HttpProxy      string `json:"HttpProxy" xml:"HttpProxy"`
-	HttpsProxy     string `json:"HttpsProxy" xml:"HttpsProxy"`
-	NoProxy        string `json:"NoProxy" xml:"NoProxy"`
-	LocalAddr      string `json:"LocalAddr" xml:"LocalAddr"`
-	MaxIdleConns   int    `json:"MaxIdleConns" xml:"MaxIdleConns"`
-	credential     credentials.Credential
+	RegionId             string `json:"RegionId" xml:"RegionId"`
+	Protocol             string `json:"Protocol" xml:"Protocol"`
+	Endpoint             string `json:"Endpoint" xml:"Endpoint"`
+	UserAgent            string `json:"UserAgent" xml:"UserAgent"`
+	SecurityToken        string `json:"SecurityToken" xml:"SecurityToken"`
+	ReadTimeout          int    `json:"ReadTimeout" xml:"ReadTimeout"`
+	ConnectTimeout       int    `json:"ConnectTimeout" xml:"ConnectTimeout"`
+	HttpProxy            string `json:"HttpProxy" xml:"HttpProxy"`
+	HttpsProxy           string `json:"HttpsProxy" xml:"HttpsProxy"`
+	NoProxy              string `json:"NoProxy" xml:"NoProxy"`
+	LocalAddr            string `json:"LocalAddr" xml:"LocalAddr"`
+	MaxIdleConns         int    `json:"MaxIdleConns" xml:"MaxIdleConns"`
+	EndpointType         string `json:"EndpointType" xml:"EndpointType"`
+	OpenPlatformEndpoint string `json:"OpenPlatformEndpoint" xml:"OpenPlatformEndpoint"`
+	credential           credentials.Credential
 }
 
 var defaultUserAgent = fmt.Sprintf("AlibabaCloud (%s; %s) Golang/%s Core/%s", runtime.GOOS, runtime.GOARCH, strings.Trim(runtime.Version(), "go"), "0.01")
@@ -37,6 +39,16 @@ func (client *BaseClient) InitClient(config map[string]interface{}) (err error) 
 	client.RegionId = getStringValue(config["regionId"])
 	client.Protocol = getStringValue(config["protocol"])
 	client.Endpoint = getStringValue(config["endpoint"])
+	client.UserAgent = getStringValue(config["userAgent"])
+	client.ReadTimeout = getIntValue(config["readTimeout"])
+	client.ConnectTimeout = getIntValue(config["connectTimeout"])
+	client.HttpProxy = getStringValue(config["httpProxy"])
+	client.HttpsProxy = getStringValue(config["httpsProxy"])
+	client.NoProxy = getStringValue(config["noProxy"])
+	client.LocalAddr = getStringValue(config["localAddr"])
+	client.MaxIdleConns = getIntValue(config["maxIdleConns"])
+	client.EndpointType = getStringValue(config["endpointType"])
+	client.OpenPlatformEndpoint = getStringValue(config["openPlatformEndpoint"])
 	conf := &credentials.Configuration{
 		AccessKeyID:     getStringValue(config["accessKeyId"]),
 		AccessKeySecret: getStringValue(config["accessKeySecret"]),
